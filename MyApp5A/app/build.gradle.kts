96f2b7733ba536927a94afaaddf178a32281ed13
jhonny-czk <jhonny.czeck@gmail.com>
2023-11-27 19:48:16 -0300

    Update

--- MyApp5A/app/build.gradle.kts
+++ MyApp5A/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -36,7 +38,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -44,4 +45,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    //kapt("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
 }
--- MyApp5A/app/src/main/java/com/example/myapp5a/MainActivity.kt
+++ MyApp5A/app/src/main/java/com/example/myapp5a/MainActivity.kt
@@ -1,11 +1,45 @@
-package com.example.myapp5a
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.myapp5a.FilmesFragment
+import com.example.myapp5a.PerfilFragment
+import com.example.myapp5a.PersonagensFragment
+import com.example.myapp5a.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_personagens -> {
+                    loadFragment(PersonagensFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.navigation_filmes -> {
+                    loadFragment(FilmesFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.navigation_perfil -> {
+                    loadFragment(PerfilFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> false
+            }
+        }
+        // Carregar o fragmento inicial (por exemplo, PersonagensFragment)
+        loadFragment(PersonagensFragment())
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
     }
 }
